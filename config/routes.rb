33f28e5716748009d9114551fbb1cbdfff7a9455
Colin Froggatt <colin@froggatt.com>
2018-01-08 13:04:02 +0000

    added friendship tablwe andmodel;

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'search_stocks', to: "stocks#search"
   resources :user_stocks, only: [:create, :destroy]
   
+  get 'my_friends', to: "users#my_friends"
   
   
   
